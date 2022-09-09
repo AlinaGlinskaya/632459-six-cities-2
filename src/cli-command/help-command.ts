eca65bd99826dc89521e7122e87ea412ccdd4d05
Alina <alinascarlett@mail.ru>
2022-09-09 12:49:58 +0300

    feat: add version-command, help-command

---
+++ src/cli-command/help-command.ts
@@ -0,0 +1,20 @@
+import { CliCommandInterface } from './cli-command.interface.js';
+
+export default class HelpCommand implements CliCommandInterface {
+  public name = '--help';
+
+  public async execute() {
+    console.log(`
+      Программа для подготовки данных для REST API сервера.
+
+      Пример вызова команды:
+        main.js --<command> [--arguments]
+
+      Команды:
+        --help:                       ## команда по умолчанию, выводит данное сообщение
+        --version:                    ## выводит номер версии
+        --import <path>:              ## импортирует данные из TSV
+        --generator <n> <path> <url>  ## генерирует указанное количество тестовых данных
+    `);
+  }
+}
