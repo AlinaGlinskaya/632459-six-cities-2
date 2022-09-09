9cc5356d2b70bd8fc72dae1285d1db1126e97433
Alina <alinascarlett@mail.ru>
2022-09-09 15:13:58 +0300

    feat: add methods for parsing and processing commands

---
+++ src/cli.ts
@@ -0,0 +1,10 @@
+import CLIApplication from './app/cli-application.js';
+import VersionCommand from './cli-command/version-command.js';
+import HelpCommand from './cli-command/help-command.js';
+
+const myManager = new CLIApplication();
+myManager.registerCommands([
+  new VersionCommand,  new HelpCommand
+]);
+myManager.processCommand(process.argv);
+
