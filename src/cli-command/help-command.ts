dce092fee9eb680f0a37ba37439db1b73979fea0
Alina <alinascarlett@mail.ru>
2022-09-10 23:48:41 +0300

    feat: add chalk package and color logs

--- src/cli-command/help-command.ts
+++ src/cli-command/help-command.ts
@@ -1,20 +1,21 @@
 import { CliCommandInterface } from './cli-command.interface.js';
+import chalk from 'chalk';
 
 export default class HelpCommand implements CliCommandInterface {
   public readonly name = '--help';
 
   public async execute(): Promise<void> {
     console.log(`
-      Программа для подготовки данных для REST API сервера.
+      ${chalk.white.bgGreen.bold('Программа для подготовки данных для REST API сервера.')}
 
-      Пример вызова команды:
-        main.js --<command> [--arguments]
+      ${chalk.green('Пример вызова команды:')}
+        ${chalk.yellow('main.js')} ${chalk.blue('--<command> [--arguments]')}
 
-      Команды:
-        --help:                       ## команда по умолчанию, выводит данное сообщение
-        --version:                    ## выводит номер версии
-        --import <path>:              ## импортирует данные из TSV
-        --generator <n> <path> <url>  ## генерирует указанное количество тестовых данных
+      ${chalk.green('Команды:')}
+        ${chalk.blue('--help:')}                       ## команда по умолчанию, выводит данное сообщение
+        ${chalk.blue('--version:')}                    ## выводит номер версии
+        ${chalk.blue('--import <path>:')}              ## импортирует данные из TSV
+        ${chalk.blue('--generator <n> <path> <url>')}  ## генерирует указанное количество тестовых данных
     `);
   }
 }
