85ef61dc10073813d67d93500a3262346639a5a3
Alina <alinascarlett@mail.ru>
2022-09-24 21:11:23 +0300

    feat: add logging and app-initialization

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import LoggerService from './common/logger/logger.service.js';
+import Application from './app/application.js';
+
+const logger = new LoggerService();
+const application = new Application(logger);
+await application.init();
