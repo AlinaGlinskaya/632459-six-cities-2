84e927d11d57d5996385dac1fa708c43009809a6
Alina <alinascarlett@mail.ru>
2022-09-29 15:29:06 +0300

    feat(docker): add db-creating in docker-compose WIP

---
+++ mongo-init.js
@@ -0,0 +1,9 @@
+db = db.getSiblingDB('six-cities');
+db.createUser(
+  {
+    user: 'admin',
+    pwd: 'pass',
+    roles: [{ role: 'readWrite', db: 'six-cities' }],
+  },
+);
+db.createCollection('users');
