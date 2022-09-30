84d0610b980053b85270d119e1c5a66222eb5185
Alina <alinascarlett@mail.ru>
2022-09-30 19:26:47 +0300

    feat: add env variables for database

--- mongo-init.js
+++ mongo-init.js
@@ -1,9 +1,9 @@
-db = db.getSiblingDB('six-cities');
-db.createUser(
-  {
-    user: 'admin',
-    pwd: 'pass',
-    roles: [{ role: 'readWrite', db: 'six-cities' }],
-  },
-);
-db.createCollection('users');
+// db = db.getSiblingDB('six-cities');
+// db.createUser(
+//   {
+//     user: 'admin',
+//     pwd: 'pass',
+//     roles: [{ role: 'readWrite', db: 'six-cities' }],
+//   },
+// );
+// db.createCollection('users');
