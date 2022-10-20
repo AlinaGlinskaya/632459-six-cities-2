e68a48118f608850c4a1d97681aa2234e8eeb055
Alina <alinascarlett@mail.ru>
2022-10-20 14:56:29 +0300

    feat: add router-interface and controller-interface

--- src/app/application.ts
+++ src/app/application.ts
@@ -5,13 +5,19 @@ import { Component } from '../types/component.types.js';
 import 'reflect-metadata';
 import { DatabaseInterface } from '../common/database-client/database.interface.js';
 import { getURI } from '../utils/db.js';
+import express, {Express} from 'express';
+import chalk from 'chalk';
 
 @injectable()
 export default class Application {
+  private expressApp: Express;
+
   constructor(
     @inject(Component.LoggerInterface) private logger: LoggerInterface,
     @inject(Component.ConfigInterface) private config: ConfigInterface,
-    @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface) {}
+    @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface) {
+    this.expressApp = express();
+  }
 
   public async init() {
     this.logger.info('Application initialization...');
@@ -27,5 +33,7 @@ export default class Application {
     );
 
     await this.databaseClient.connect(uri);
+    this.expressApp.listen(this.config.get('PORT'));
+    this.logger.info(chalk.blue(`Server started on http://localhost:${this.config.get('PORT')}`));
   }
 }
