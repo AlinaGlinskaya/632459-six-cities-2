ea4b21f34dcd2912afdd11c1a46ca8995795bdd8
Alina <alinascarlett@mail.ru>
2022-10-22 12:53:49 +0300

    feat: add auth methods to user-controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -11,6 +11,7 @@ import { fillDTO } from '../../utils/common.js';
 import CreateUserDto from './dto/create-user.dto.js';
 import { UserServiceInterface } from './user-service.interface.js';
 import UserResponse from './response/user.response.js';
+import LoginUserDto from './dto/login-user.dto.js';
 
 @injectable()
 export default class UserController extends Controller {
@@ -23,6 +24,9 @@ export default class UserController extends Controller {
 
     this.logger.info('Register routes for UserController...');
     this.addRoute({path: '/register', method: HttpMethod.Post, handler: this.create});
+    this.addRoute({path: '/login', method: HttpMethod.Post, handler: this.login});
+    this.addRoute({path: '/login', method: HttpMethod.Get, handler: this.checkAuth});
+    this.addRoute({path: '/logout', method: HttpMethod.Post, handler: this.logout});
   }
 
   public async create(
@@ -40,11 +44,60 @@ export default class UserController extends Controller {
     }
 
     const result = await this.userService.create(body, this.configService.get('SALT'));
-    this.send(
-      res,
-      StatusCodes.CREATED,
-      fillDTO(UserResponse, result)
+    this.created(
+      res, fillDTO(UserResponse, result)
     );
 
+  }
+
+  public async login(
+    {body}:Request<Record<string, unknown>, Record<string,unknown>, LoginUserDto>,
+    res: Response): Promise<void> {
+
+    const existUser = await this.userService.findByEmail(body.email);
+
+    if(!existUser) {
+      throw new HttpError(
+        StatusCodes.UNAUTHORIZED,
+        `User with email ${body.email} not found`,
+        'UserController'
+      );
+    }
+
+    const token = 'someToken';
+    this.ok(res, token);
+  }
+
+  public async checkAuth(
+    {body}:Request<Record<string, unknown>, Record<string,unknown>>,
+    res: Response): Promise<void> {
+    const existUser = await this.userService.findByEmail(body.email);
+
+    if (!existUser) {
+      throw new HttpError(
+        StatusCodes.UNAUTHORIZED,
+        `User with email ${body.email} unauthorised`,
+        'UserController'
+      );
+    }
+
+    this.ok(res, fillDTO(UserResponse, existUser));
+  }
+
+  public async logout(
+    {body}:Request<Record<string, unknown>, Record<string,unknown>>,
+    res: Response): Promise<void> {
+
+    const existUser = await this.userService.findByEmail(body.email);
+
+    if (!existUser) {
+      throw new HttpError(
+        StatusCodes.UNAUTHORIZED,
+        `User with email ${body.email} unauthorised`,
+        'UserController'
+      );
+    }
+
+    this.noContent(res);
   }
 }
