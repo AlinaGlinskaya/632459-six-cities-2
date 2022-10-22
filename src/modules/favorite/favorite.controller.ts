31eea191166b09f645b8dccff8884d1dde3c476e
Alina <alinascarlett@mail.ru>
2022-10-22 18:26:33 +0300

    feat: add favorite handlers

--- src/modules/favorite/favorite.controller.ts
+++ src/modules/favorite/favorite.controller.ts
@@ -9,8 +9,15 @@ import { Request, Response } from 'express';
 import * as core from 'express-serve-static-core';
 import { fillDTO } from '../../utils/common.js';
 import OfferResponse from '../offer/response/offer.response.js';
+import HttpError from '../../common/errors/http-error.js';
+import { StatusCodes } from 'http-status-codes';
 
-type ParamsFavoriteOffer = {
+type ParamsGetFavorite = {
+  userId: string
+}
+
+type ParamsChangeFavorite = {
+  offerId: string,
   userId: string
 }
 
@@ -24,9 +31,11 @@ export default class FavoriteController extends Controller {
 
     this.logger.info('Register routes for FavoriteController...');
     this.addRoute({path: '/:userId', method: HttpMethod.Get, handler: this.index});
+    this.addRoute({path: '/:userId/:offerId', method: HttpMethod.Post, handler: this.addFavorite});
+    this.addRoute({path: '/:userId/:offerId', method: HttpMethod.Delete, handler: this.removeFavorite});
   }
 
-  public async index({params}: Request<core.ParamsDictionary | ParamsFavoriteOffer>, res: Response): Promise<void> {
+  public async index({params}: Request<core.ParamsDictionary | ParamsGetFavorite>, res: Response): Promise<void> {
     const {userId} = params;
 
     const user = await this.userService.findFavoritesIds(userId);
@@ -38,5 +47,35 @@ export default class FavoriteController extends Controller {
       const offers = await this.offerService.findFavoriteByIds(favoriteIds);
       this.ok(res, fillDTO(OfferResponse, offers));
     }
+  }
+
+  public async addFavorite({params}: Request<core.ParamsDictionary | ParamsChangeFavorite>, res: Response): Promise<void> {
+    const {userId, offerId} = params;
+    const offer = await this.userService.addToFavorites(userId, offerId);
+
+    if (!offer) {
+      throw new HttpError(
+        StatusCodes.NOT_FOUND,
+        `Offer with id ${offerId} not found`,
+        'Favorite Controller'
+      );
+    }
+
+    this.noContent(res, offer);
+  }
+
+  public async removeFavorite({params}: Request<core.ParamsDictionary | ParamsChangeFavorite>, res: Response): Promise<void> {
+    const {userId, offerId} = params;
+    const offer = await this.userService.removeFromFavorites(userId, offerId);
+
+    if (!offer) {
+      throw new HttpError(
+        StatusCodes.NOT_FOUND,
+        `Offer with id ${offerId} not found`,
+        'Favorite Controller'
+      );
+    }
+
+    this.noContent(res, offer);
   }
 }
